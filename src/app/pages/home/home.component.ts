6af3d818866aebdc5843465ca3021c1714252458
Raaaaaaaaaj <143274616+Raaaaaaaaaj@users.noreply.github.com>
2025-03-05 13:51:01 +0530

    Deleted Existing Theme
    deleted Existing theme and changed font face

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { ServiceCardsComponent } from '../../commonComponents/service-cards/service-cards.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ServiceCardsComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
